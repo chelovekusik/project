5f3ace7d752090c9b9a6eed09edeaa2a518e1a0d
Boris <example@gmail.com>
2020-07-09 20:05:07 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+
+/*   007 urok
+const result = confirm("Are you here?");
+
+console.log(result); */
+
+/* const answer = +prompt("Есть ли вам 18 лет", "Пиши сучка");
+console.log(answer + 5); */
+
+/* const answers = [];
+
+answers[0] = prompt('Ваш возраст', '');
+answers[1] = prompt('Ваше имя', '');
+answers[2] = prompt('Ваш номер телефона', 'номер');
+
+/* console.log(answers); */
+/* document.write(answers);
+
+console.log(typeof(answers)); */ 
+
+/* //008
+
+const user = 'Ivan';
+
+alert(`Привет ${user}`);// */
+
+//009
+
+let incr = 10,
+    decr = 10;
+
+/* ++incr;
+--decr; */
+
+console.log(++incr);
+console.log(--decr);
